ad7102fa2e1816b21b7edba05c055a7b86a5f241
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-10-06 08:12:50 +0430

    create category_product migration

---
+++ database/migrations/2022_09_29_112433_create_category_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id');        
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('category_id')->on('categories')->references('id');
+            $table->foreign('product_id')->on('products')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_product');
+    }
+};
