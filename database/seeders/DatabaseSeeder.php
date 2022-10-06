812e752abdaba8a95b1ca72227f00087aa1f4476
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-10-06 09:26:47 +0430

    update database seeder fix category_product bug

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Transaction;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +19,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        User::truncate();
+        Transaction::truncate();
+        Product::truncate();
+        Category::truncate();
+        DB::table('category_product')->truncate();
+
+        $user_quantity = 200;
+        $category_quantity = 20;
+        $product_quantity = 500;
+        $transaction_quantity = 500;
+
+        User::factory($user_quantity)->create();
+        
+        Category::factory($category_quantity)->create();
+        
+        Product::factory($product_quantity)->create()
+        ->each(function($product){
+            $categories = Category::all()->random(mt_rand(1,4))->pluck('id');
+            $product->categories()->attach($categories); 
+        });
+       
+        Transaction::factory($transaction_quantity)->create();
+    
     }
 }
