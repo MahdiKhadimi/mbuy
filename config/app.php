ce95195313f4113592853ab25409548897c11f47
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-10-09 19:42:21 +0430

    Update laravel and laravel homestead, laravel fractal, laravel passport and laravel cors have been added

--- config/app.php
+++ config/app.php
@@ -185,10 +185,11 @@ return [
         /*
          * Package Service Providers...
          */
-
+       
         /*
          * Application Service Providers...
          */
+    
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
