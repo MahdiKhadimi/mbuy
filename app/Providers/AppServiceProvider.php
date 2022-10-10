6bebf44294fd5be0f295e7c81b7f55d23dfae96c
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-10-10 10:49:24 +0430

    create user updated and user created listener to send email to user and create product updated event listener to upadate product quantity after each transaction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Models\Product;
+use App\Mail\UserCreated;
+use App\Mail\UserMailChange;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +29,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        User::created(function($user){
+            retry(5,function() use($user){
+              Mail::to($user->email)->send(new UserCreated($user));
+            },100);
+        });
+        
+        User::updated(function($user){
+            if($user->isDirty('email')){
+             retry(5,function() use($user){
+                Mail::to($user->email)->send(new UserMailChange($user));
+             },100);
+            }
+         });
+         
+         Product::updated(function($product){
+              if($product->quantity==0 && $product->is_available()){
+                $product->status = Product::UNAVAILABLE_PRODUCT; 
+                $product->save();  
+              }  
+         });
+         
+        Schema::defaultStringLength(191);
     }
 }
