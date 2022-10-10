15fbe04f275698c21387dce0dab681cc164e3fda
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-10-10 09:16:12 +0430

    categoryTransform has been created: to show category in each transaction

---
+++ app/Http/Controllers/Category/CategoryTransactionController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Http\Controllers\ApiController;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryTransactionController extends ApiController
+{
+    public function index(Category $category)
+    {
+        $transactions = $category->products()
+        ->whereHas('transactions')
+        ->with('transactions')
+        ->get()
+        ->pluck('transactions')
+        ->unique()
+        ->values()
+        ->collapse();
+        
+
+        return $this->show_all($transactions);
+    }
+}
