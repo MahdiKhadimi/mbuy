98b8b1123d1ddd52fab4f33c094e090bc1abf806
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-10-10 09:22:09 +0430

    add show_message method in the ApiResponser trait

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -22,7 +22,7 @@ use Illuminate\Database\Eloquent\Collection;
 
     public function show_all($data,$code=200)
     {
-        return $this->success_response($data,$code);
+        return $this->success_response(['data'=>$data],$code);
     }
 
     public function show_one(Model $model,$code=200)
@@ -30,6 +30,10 @@ use Illuminate\Database\Eloquent\Collection;
         return $this->success_response(['data'=>$model],$code);
     }
     
+    public function show_message($message,$code=200)
+    {
+        return $this->success_response(['data'=>$message],$code);
+    }
 
  }
  
